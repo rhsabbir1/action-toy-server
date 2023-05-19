be2226ef9102d3df4e989d3f7dda4d5c86a6fc13
rhsabbir1 <rhsabbir090@gmai.com>
2023-05-19 12:36:38 +0600

    get all toys and sub catagories

--- index.js
+++ index.js
@@ -30,6 +30,19 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toyCollection = client.db("toyDb").collection("products");
+    
+    app.get('/products',async(req,res)=>{
+        const result = await toyCollection.find().toArray()
+        res.send(result)
+    })
+
+    app.get('/products/:text' , async(req , res)=>{
+        const result = await toyCollection.find({sub_category : req.params.text}).toArray()
+        res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
