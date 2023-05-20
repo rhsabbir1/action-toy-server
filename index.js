17ca0a0f114153d2463e2adf160dbab56a135234
rhsabbir1 <rhsabbir090@gmai.com>
2023-05-20 21:52:13 +0600

    update toys

--- index.js
+++ index.js
@@ -38,12 +38,7 @@ async function run() {
       res.send(result)
     })
 
-    app.get('/products/:text', async (req, res) => {
-      const result = await toyCollection.find({ sub_category: req.params.text }).toArray()
-      res.send(result)
-    })
-
-    app.get('/products', async (req, res) => {
+    app.get('/product', async (req, res) => {
 
       let query = {}
       if (req.query?.sellerEmail) {
@@ -52,10 +47,37 @@ async function run() {
       const result = await toyCollection.find(query).toArray()
       res.send(result)
     })
+    app.get('/update/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await toyCollection.findOne(query)
+      res.send(result)
+    })
+
+    app.get('/products/:text', async (req, res) => {
+      const result = await toyCollection.find({ sub_category: req.params.text }).toArray()
+      res.send(result)
+    })
 
     app.post('/products', async (req, res) => {
       const newToy = req.body;
       const result = await toyCollection.insertOne(newToy)
+      res.send(result)
+    })
+
+    app.put('/update/:id', async(req,res)=>{
+      const id = req.params.id;
+      const filter = {_id : new ObjectId(id)}
+      const options = { upsert: true };
+      const updateToy = req.body;
+      const toy = {
+        $set: {
+           price :updateToy.price,    
+           quantity:updateToy.quantity,
+          description:updateToy.description
+        },
+      };
+      const result = await toyCollection.updateOne(filter, toy, options);
       res.send(result)
     })
 
